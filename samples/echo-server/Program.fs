9edf74b6de4bda9e93fdeffcf060b446c565a77e
Reed Mullanix <reed.mullanix@mybazinga.com>
2017-07-28 15:04:19 -0700

    Update echo server

--- samples/echo-server/Program.fs
+++ samples/echo-server/Program.fs
@@ -6,8 +6,12 @@ open System.Threading
 let main argv =
     let rec echoServer () = socketIO {
         let! line = Socket.readSocket
-        do! Socket.writeSocket (line)
-        return! echoServer ()
+        if line.Trim() <> "QUIT"  then
+            do! Socket.writeSocket (line)
+            return! echoServer ()
+        else
+            do! Socket.writeSocket ("Goodbye!\n")
+            return ()
     }
     printfn "Starting Server..."
     Socket.runParallel 9000 100 (echoServer()) |> Async.RunSynchronously
