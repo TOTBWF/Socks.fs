643358f51d2854d924cfa2b76a7b3bf0b7c9f9b2
Reed Mullanix <reed.mullanix@mybazinga.com>
2017-07-28 14:31:02 -0700

    Added new sample

---
+++ samples/echo-server/Program.fs
@@ -0,0 +1,14 @@
+﻿open Socks
+open System.Threading
+
+
+[<EntryPoint>]
+let main argv =
+    let rec echoServer () = socketIO {
+        let! line = Socket.readSocket
+        do! Socket.writeSocket (line)
+        return! echoServer ()
+    }
+    printfn "Starting Server..."
+    Socket.runParallel 9000 100 (echoServer()) |> Async.RunSynchronously
+    0
